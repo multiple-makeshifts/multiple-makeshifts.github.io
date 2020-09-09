0b8818c19a0370ff2ea75cb0fc64f1316cc5e680
Colin Brayton <gringolalia@gmail.com>
2020-09-09 11:04:44 -0300

    deploy

---
+++ precache-manifest.7e2a1178193bf60fe29f663c5abfd5f9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797cb0d60f6929e35b4f",
+    "url": "/css/about.771193fb.css"
+  },
+  {
+    "revision": "cbd176b0a68e265cb114",
+    "url": "/css/app.57f93afd.css"
+  },
+  {
+    "revision": "d5b4bd69039d4fe31afd",
+    "url": "/css/chunk-vendors.d4c8a858.css"
+  },
+  {
+    "revision": "20a42ef12aac41cac1c8969c5758948e",
+    "url": "/fonts/inkline.20a42ef1.woff"
+  },
+  {
+    "revision": "ebe25a09f8c6844b928dae1ffa65ac79",
+    "url": "/fonts/inkline.ebe25a09.ttf"
+  },
+  {
+    "revision": "ff99d3fb75eedf667974b02c42e391d2",
+    "url": "/fonts/inkline.ff99d3fb.eot"
+  },
+  {
+    "revision": "c1962138b9c4e619113effef2e519536",
+    "url": "/img/machiine-one.c1962138.jpg"
+  },
+  {
+    "revision": "ee42ce781439fc5251447ce3ad822372",
+    "url": "/img/vintage-test-one.ee42ce78.jpg"
+  },
+  {
+    "revision": "06451347ef13f9ac9efa98c20d29fe18",
+    "url": "/img/vintage-test-three.06451347.jpg"
+  },
+  {
+    "revision": "b6c7fd8b1b6cb21f293865e4bd7becee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "797cb0d60f6929e35b4f",
+    "url": "/js/about.fd89683b.js"
+  },
+  {
+    "revision": "cbd176b0a68e265cb114",
+    "url": "/js/app.e76602df.js"
+  },
+  {
+    "revision": "d5b4bd69039d4fe31afd",
+    "url": "/js/chunk-vendors.24f6f7c8.js"
+  },
+  {
+    "revision": "557124bbfdec3ac97f795c261484cbe3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
